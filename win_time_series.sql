280bd757be2874ec9a14d8592f5b3d2890bf82ae
Rashmi Raghu <rraghu@pivotal.io>
2016-06-16 15:16:43 -0700

    cutting out the temp table step

--- win_time_series.sql
+++ win_time_series.sql
@@ -35,22 +35,22 @@ $$
         sql := 'select max(rid) from ' ||data_tab|| ';';
         EXECUTE sql INTO rid_last;
 
-        EXECUTE 'drop table if exists temp_pdltools_win_comp_id_tbl;';
-        sql := 'create temp table temp_pdltools_win_comp_id_tbl as
-            select *, win_start_id/' ||win_slide_size|| ' as win_id, win_internal_comp_id + win_start_id - 1 as win_external_comp_id from
-            (
-              select generate_series(1,' ||win_size|| ',1) as win_internal_comp_id
-            ) t1,
-            (
-              select generate_series(' ||rid_first|| ',' ||rid_last|| ',' ||win_slide_size|| ') as win_start_id
-            ) t2
-        distributed by (win_id,win_internal_comp_id);
-        ';
-        EXECUTE sql;
-
-        -- Debug the temp table
-        EXECUTE 'select count(*) from temp_pdltools_win_comp_id_tbl;' INTO ct_rows_temp_tbl;
-        RAISE NOTICE 'Count of rows in the temp table: %', ct_rows_temp_tbl;
+        -- EXECUTE 'drop table if exists temp_pdltools_win_comp_id_tbl;';
+        -- sql := 'create temp table temp_pdltools_win_comp_id_tbl as
+        --     select *, win_start_id/' ||win_slide_size|| ' as win_id, win_internal_comp_id + win_start_id - 1 as win_external_comp_id from
+        --     (
+        --       select generate_series(1,' ||win_size|| ',1) as win_internal_comp_id
+        --     ) t1,
+        --     (
+        --       select generate_series(' ||rid_first|| ',' ||rid_last|| ',' ||win_slide_size|| ') as win_start_id
+        --     ) t2
+        -- distributed by (win_id,win_internal_comp_id);
+        -- ';
+        -- EXECUTE sql;
+        --
+        -- -- Debug the temp table
+        -- EXECUTE 'select count(*) from temp_pdltools_win_comp_id_tbl;' INTO ct_rows_temp_tbl;
+        -- RAISE NOTICE 'Count of rows in the temp table: %', ct_rows_temp_tbl;
 
         sql := 'create table ' ||output_tab|| '(win_id BIGINT, arr_rid BIGINT[], arr_val FLOAT8[]) distributed by (win_id);';
         EXECUTE sql;
@@ -63,16 +63,26 @@ $$
                 array_agg(val order by win_external_comp_id) as arr_val
             from
             (
-              select * from
-                  temp_pdltools_win_comp_id_tbl t1,
-                  (select ' ||id|| ',' ||val|| ' from ' ||data_tab|| ') t2
-              where t1.win_external_comp_id = t2.' ||id|| '
+                select * from
+                    (
+                        select *, win_start_id/' ||win_slide_size|| ' as win_id, win_internal_comp_id + win_start_id - 1 as win_external_comp_id from
+                        (
+                          select generate_series(1,' ||win_size|| ',1) as win_internal_comp_id
+                        ) ta,
+                        (
+                          select generate_series(' ||rid_first|| ',' ||rid_last|| ',' ||win_slide_size|| ') as win_start_id
+                        ) tb
+                    ) t1,
+                    (
+                        select ' ||id|| ',' ||val|| ' from ' ||data_tab|| '
+                    ) t2
+                where t1.win_external_comp_id = t2.' ||id|| '
             ) t3
             group by win_id;
         ';
         EXECUTE sql;
 
-        EXECUTE 'drop table if exists temp_pdltools_win_comp_id_tbl;';
+        -- EXECUTE 'drop table if exists temp_pdltools_win_comp_id_tbl;';
 
         RETURN;
      END;
